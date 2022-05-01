ef90be898e68cfc3192e2d2fcf4802879643c265
ProgrammerAuditore <victorvj098@gmail.com>
2022-05-01 11:55:49 -0500

    *** Agregado: Express-handlebars

--- app/index.js
+++ app/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const exhbs = require('express-handlebars');
 const path = require('path');
+const indexRoutes = require('./routes/index.routes');
 
 app.set('port', 3078);
+
+app.set('views', path.join(__dirname,'views'));
+
+app.engine('.hbs', exhbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname,"views","layouts"),
+    partialsDir: path.join(__dirname,"views","partials"),
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+
+app.use(indexRoutes);
 
 app.use(express.static(path.join(__dirname,'public')));
 
